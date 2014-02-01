2a4b4d80a906617196d2b69a6df17d2a688c6737
Randall Rushing <randall.rushing@gmail.com>
2014-02-01 03:50:56 -0500

    Navigation Bar size scaling
    Summary: make it big or small
    
    Test Plan: we already have the framework, bring the settings back.
    
    Reviewers: synergy, anders3408, jriordan001, raymanfx, donald.munn, rlasalle15
    
    Differential Revision: http://review.codefirex.com/D358

--- src/com/android/settings/cyanogenmod/SystemUiSettings.java
+++ src/com/android/settings/cyanogenmod/SystemUiSettings.java
@@ -17,14 +17,18 @@
 package com.android.settings.cyanogenmod;
 
 import org.codefirex.utils.CFXConstants;
+import org.codefirex.utils.CFXUtils;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.UserHandle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.provider.Settings;
 
 import com.android.settings.R;
@@ -35,12 +39,15 @@ public class SystemUiSettings extends SettingsPreferenceFragment implements
 	private static final String TAG = "SystemSettings";
 	private static final String NET_VISIBLE = "cfx_netstats_visible";
 	private static final String NET_INTERVAL = "cfx_netstats_refresh_interval";
+	private static final String CAT_NAVIGATION ="cfx_systemui_navigation_bar";
+	private static final String NAVBAR_SIZE = "cfx_interface_navbar_size";
 
 	ContentResolver mResolver;
 	Context mContext;
 
 	private CheckBoxPreference mVisible;
 	private ListPreference mInterval;
+	private ListPreference mNavbarSize;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -60,6 +67,22 @@ public class SystemUiSettings extends SettingsPreferenceFragment implements
 				CFXConstants.STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL, 500);
 		mInterval.setValue(String.valueOf(currentVal));
 		mInterval.setOnPreferenceChangeListener(this);
+
+		if (!CFXUtils.hasNavBar(mContext)) {
+			getPreferenceScreen().removePreference(
+					(PreferenceCategory) getPreferenceScreen().findPreference(
+							CAT_NAVIGATION));
+		} else {
+			mNavbarSize = (ListPreference) findPreference(NAVBAR_SIZE);
+			int sizeVal = Settings.System.getInt(mResolver,
+					CFXConstants.SYSTEMUI_NAVBAR_SIZE_DP,
+					CFXConstants.SYSTEMUI_NAVBAR_SIZE_DEF_INDEX);
+			mNavbarSize.setDefaultValue(String.valueOf(sizeVal));
+			mNavbarSize.setValue(String.valueOf(sizeVal));
+			mNavbarSize.setOnPreferenceChangeListener(this);
+			updateSizeSummary();
+		}
+
 		updateIntervalSummary();
 
 	}
@@ -76,8 +99,7 @@ public class SystemUiSettings extends SettingsPreferenceFragment implements
 					CFXConstants.STATUS_BAR_NETWORK_STATS,
 					((Boolean) newValue).booleanValue());
 			return true;
-		}
-		if (preference.equals(mInterval)) {
+		} else if (preference.equals(mInterval)) {
 			Settings.System.putLong(mResolver,
 					CFXConstants.STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL,
 					Long.parseLong(String.valueOf(newValue)));
@@ -87,6 +109,23 @@ public class SystemUiSettings extends SettingsPreferenceFragment implements
 					updateIntervalSummary();
 				}
 			}, 100);
+			return true;
+		} else if (preference.equals(mNavbarSize)) {
+			int val = Integer.parseInt(((String) newValue).toString());
+			Settings.System.putInt(mResolver,
+					CFXConstants.SYSTEMUI_NAVBAR_SIZE_DP, (val));
+			Intent intent = new Intent().setAction(
+					CFXConstants.ACTION_CFX_UI_CHANGE).putExtra(
+					CFXConstants.INTENT_REASON_UI_CHANGE,
+					CFXConstants.INTENT_REASON_UI_BAR_SIZE);
+			mContext.sendBroadcastAsUser(intent, new UserHandle(
+					UserHandle.USER_ALL));
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					updateSizeSummary();
+				}
+			}, 250);
 			return true;
 		}
 		return false;
@@ -106,5 +145,21 @@ public class SystemUiSettings extends SettingsPreferenceFragment implements
 			}
 		}
 		mInterval.setSummary(newEntry);
+	}
+
+	private void updateSizeSummary() {
+		String[] entries = mContext.getResources().getStringArray(
+				R.array.systemui_navbar_size_entries);
+		String[] vals = mContext.getResources().getStringArray(
+				R.array.systemui_navbar_size_values);
+		String currentVal = mNavbarSize.getValue();
+		String newEntry = "";
+		for (int i = 0; i < vals.length; i++) {
+			if (vals[i].equals(currentVal)) {
+				newEntry = entries[i];
+				break;
+			}
+		}
+		mNavbarSize.setSummary(newEntry);
 	}
 }
