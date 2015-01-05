1b6faba5e5623c1d18196cc190e05e50f8bbb5b7
Ricardo Cerqueira <ricardo@cyngn.com>
2015-01-05 15:03:31 +0000

    Utils: Fix isPackageInstalled
    The original implementation ignored the package state, and this is
    necessary for enable/disable toggles like the one used by Terminal.
    
    Bring the original behavior back, and make the new one optional
    
    Conflicts:
    
    	src/com/android/settings/DevelopmentSettings.java
    	src/com/android/settings/SettingsActivity.java
    	src/com/android/settings/Utils.java
    
    Change-Id: I7a71f26322f4fa76dc2ae8664b3065bd6719cf11

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -341,7 +341,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             disableForUser(mQuickBoot);
         }
 
-        if (!isPackageInstalled(getActivity(), QUICKBOOT_PACKAGE_NAME)) {
+        if (!Utils.isPackageInstalled(getActivity(), QUICKBOOT_PACKAGE_NAME, false)) {
             removePreference(mQuickBoot);
         }
 
--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1017,4 +1017,23 @@ public final class Utils {
         }
         return sb.toString();
     }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
+    }
 }
