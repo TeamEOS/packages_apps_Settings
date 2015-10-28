94b56bc3b94e10ad8d7017d0e9c8308897465a9e
Roman Birg <roman@cyngn.com>
2015-10-28 07:25:52 +0000

    Add quick settings pull down with one finger [2/2]
    Change-Id: I87f6255b60564019cb966749ea921a791744f460

--- src/com/android/settings/eos/StatusbarSettings.java
+++ src/com/android/settings/eos/StatusbarSettings.java
@@ -16,12 +16,29 @@
 package com.android.settings.eos;
 
 import android.os.Bundle;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
+import com.android.settings.Utils;
 
-public class StatusbarSettings extends SettingsPreferenceFragment {
+public class StatusbarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
+    private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
+
+    private ListPreference mQuickPulldown;
 
     @Override
     protected int getMetricsCategory() {
@@ -32,5 +49,48 @@ public class StatusbarSettings extends SettingsPreferenceFragment {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.statusbar_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+	mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
+        if (!Utils.isPhone(getActivity())) {
+            prefSet.removePreference(mQuickPulldown);
+        } else {
+            // Quick Pulldown
+            mQuickPulldown.setOnPreferenceChangeListener(this);
+            int statusQuickPulldown = Settings.System.getInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);
+            mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
+            updateQuickPulldownSummary(statusQuickPulldown);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mQuickPulldown) {
+            int statusQuickPulldown = Integer.valueOf((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+                    statusQuickPulldown);
+            updateQuickPulldownSummary(statusQuickPulldown);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateQuickPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // quick pulldown deactivated
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
+        } else {
+            Locale l = Locale.getDefault();
+            boolean isRtl = TextUtils.getLayoutDirectionFromLocale(l) == View.LAYOUT_DIRECTION_RTL;
+            String direction = res.getString(value == 2
+                    ? (isRtl ? R.string.quick_pulldown_right : R.string.quick_pulldown_left)
+                    : (isRtl ? R.string.quick_pulldown_left : R.string.quick_pulldown_right));
+            mQuickPulldown.setSummary(res.getString(R.string.summary_quick_pulldown, direction));
+        }
     }
 }
